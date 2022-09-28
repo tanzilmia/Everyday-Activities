add029951239c0a3f0586073a132b15c349f063d
tanzilmia <ziltan590@gmail.com>
2022-09-28 23:02:13 +0600

    home page add

---
+++ src/component/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css'
+const Home = () => {
+    return (
+        <div className='home_container'>
+            <div className='activitites_container'>
+                
+                
+            </div>
+
+            <div className='profile_info'>
+                <h2>Profile info</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
